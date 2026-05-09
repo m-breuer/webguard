eb4068cf7313861ced9874ce6e13e470af78f179
Marcel Breuer <breuer.marcel@outlook.com>
2026-05-09 11:00:47 +0200

    fix: noindex auth entry pages

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,7 +14,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="robots" content="index, follow">
+    <meta name="robots" content="noindex, follow">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     {!! $head ?? '' !!}
