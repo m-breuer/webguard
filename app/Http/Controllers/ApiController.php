cc4cd5a109e3f29b5b5968c041a89338990e845f
Marcel Breuer <breuer.marcel@outlook.com>
2026-05-27 00:04:27 +0200

    fix: document widget ninety day uptime

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -356,6 +356,7 @@ class ApiController extends Controller
      *   "uptime": {
      *     "7_days": 100,
      *     "30_days": 99.9,
+     *     "90_days": 99.5,
      *     "365_days": 99.1
      *   },
      *   "public_url": "https://example.com/label/01H...",
