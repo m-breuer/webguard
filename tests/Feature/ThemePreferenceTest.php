ddf3e2d53e6b0de38e0e31ea41a4f2bca635b329
Marcel Breuer <breuer.marcel@outlook.com>
2026-08-10 00:07:13 +0200

    test(theme): cover bootstrap layouts

--- tests/Feature/ThemePreferenceTest.php
+++ tests/Feature/ThemePreferenceTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Tests\Feature;
 
+use App\Models\Package;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -28,5 +30,15 @@ class ThemePreferenceTest extends TestCase
         $testResponse->assertSeeHtml("html.classList.toggle('dark', isDark);");
         $testResponse->assertSeeHtml('html.dark {');
         $testResponse->assertSeeHtml('background-color: #020617;');
+    }
+
+    public function test_authenticated_layout_includes_theme_bootstrap_before_assets_load(): void
+    {
+        $user = User::factory()->for(Package::factory())->create();
+
+        $testResponse = $this->actingAs($user)->get(route('dashboard'));
+
+        $testResponse->assertOk();
+        $testResponse->assertSeeHtml('<meta name="color-scheme" content="light dark">');
     }
 }
