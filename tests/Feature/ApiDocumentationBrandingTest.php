2653b86dbd27be0ee2add54ae9dddfd91b8438a3
Marcel Breuer <breuer.marcel@outlook.com>
2026-09-04 08:49:45 +0200

    fix(api-docs): use root-relative logo path

--- tests/Feature/ApiDocumentationBrandingTest.php
+++ tests/Feature/ApiDocumentationBrandingTest.php
@@ -10,9 +10,9 @@ class ApiDocumentationBrandingTest extends TestCase
 {
     public function test_scribe_uses_a_publicly_deployed_logo_asset(): void
     {
-        $logoPath = 'images/Logo-WebGuard.png';
+        $logoPath = '/images/Logo-WebGuard.png';
 
         $this->assertSame($logoPath, config('scribe.logo'));
-        $this->assertFileExists(public_path($logoPath));
+        $this->assertFileExists(public_path(mb_ltrim($logoPath, '/')));
     }
 }
